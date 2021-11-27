016f221a892bfbae9203bf72396cd80229306705
sourovahmad <mdsourovahmad19@gmail.com>
2021-11-27 22:56:17 +0600

    livewire init

---
+++ database/factories/StudentsFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StudentsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'firstname' => $this->faker->firstName(),
+            'lastname' => $this->faker->lastName(),
+            'email' => $this->faker->email(),
+            'address' => $this->faker->address(),
+        ];
+    }
+}
